e11eacd47965793a2fb8ea6b5bca6408aba8d304
leonardoespinosa <leonardoespinosa@MacBook-Pro-de-Leonardo.local>
2017-06-01 00:49:08 -0500

    Add functions in colombia payment

--- qmo_web/client/imports/app/web/customer/payments/country-payment/colombia-payment/colombia-payment.component.ts
+++ qmo_web/client/imports/app/web/customer/payments/country-payment/colombia-payment/colombia-payment.component.ts
@@ -4,11 +4,12 @@ import { MeteorObservable } from "meteor-rxjs";
 import { TranslateService } from 'ng2-translate';
 import { Subscription, Observable } from 'rxjs';
 import { Orders } from "../../../../../../../../both/collections/restaurant/order.collection";
-import { Order } from '../../../../../../../../both/models/restaurant/order.model';
+import { Order, OrderTranslateInfo } from '../../../../../../../../both/models/restaurant/order.model';
 import { Currency } from '../../../../../../../../both/models/general/currency.model';
 import { Currencies } from '../../../../../../../../both/collections/general/currency.collection';
 import { Restaurant } from '../../../../../../../../both/models/restaurant/restaurant.model';
 import { Restaurants } from '../../../../../../../../both/collections/restaurant/restaurant.collection';
+import { ColombiaPaymentDetailComponent } from './colombia-payment-detail/colombia-payment-detail.component';
 
 import template from './colombia-payment.component.html';
 import style from './colombia-payment.component.scss';
@@ -26,6 +27,7 @@ export class ColombiaPaymentComponent implements OnInit, OnDestroy {
 
     private _user = Meteor.userId();
     private _totalValue    : number = 0;
+    public _dialogRef      : MdDialogRef<any>;
 
     private _ordersSubscription : Subscription;
     private _currencySub        : Subscription;
@@ -38,18 +40,21 @@ export class ColombiaPaymentComponent implements OnInit, OnDestroy {
     private _ipoComValue        : number = 0;
     private _ipoComBaseValue    : number = 0;
     private _totalToPayment     : number = 0;
+    private _otherTip           : number = 0;
+    private _oldOtherTip        : number = 0;
     private _ipoComBaseString   : string;
     private _ipoComString       : string;
     private _tipTotalString     : string;
     private _currencyCode       : string;
     private _tipValue           : string;
+    private _otherTipAllowed    : boolean = true;
 
     /**
      * ColombiaPaymentComponent Constructor
      * @param {TranslateService} _translate 
      * @param {NgZone} _ngZone 
      */
-    constructor( private _translate: TranslateService, private _ngZone:NgZone ) {
+    constructor( private _translate: TranslateService, private _ngZone:NgZone, public _dialog: MdDialog ) {
         var _userLang = navigator.language.split( '-' )[0];
         _translate.setDefaultLang( 'en' );
         _translate.use( _userLang );
@@ -82,6 +87,7 @@ export class ColombiaPaymentComponent implements OnInit, OnDestroy {
         Orders.collection.find( { creation_user: this._user, status: 'ORDER_STATUS.DELIVERED' } ).fetch().forEach( ( order ) => {
             this._totalValue += order.totalPayment;
         });
+
         let _lRestaurant:Restaurant = Restaurants.findOne( { _id: this.restId } );
         if( _lRestaurant.financialInformation[ "TIP_PERCENTAGE" ] > 0 ){
             this._tipValue = _lRestaurant.financialInformation[ "TIP_PERCENTAGE" ];
@@ -95,7 +101,96 @@ export class ColombiaPaymentComponent implements OnInit, OnDestroy {
         this._ipoComString     = (this._ipoComValue).toFixed(2);
 
         this._tipTotalString   = (this._tipTotal).toFixed(2);
-        this._totalToPayment   = this._totalValue + this._tipTotal;
+        this._totalToPayment   = this._totalValue;
+    }
+
+    /**
+     * Function to evaluate if the order is available to return to the first owner
+     * @param {Order} _pOrder
+     */
+    isAvailableToReturn( _pOrder:Order ):boolean{
+        if( _pOrder.translateInfo.firstOrderOwner !== '' && _pOrder.translateInfo.lastOrderOwner !== '' 
+            && _pOrder.translateInfo.confirmedToTranslate && _pOrder.translateInfo.markedToTranslate ){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * When this user want return the order, this function allow return the order with the original owner
+     * @param {Order} _pOrder 
+     */
+    returnOrderToFirstOwner( _pOrder:Order ):void{
+        if( confirm( 'Desea devolver la orden al usuario ' + _pOrder.translateInfo.firstOrderOwner + ' ?' ) ) {
+            let _lOrderTranslateInfo: OrderTranslateInfo = { firstOrderOwner: _pOrder.translateInfo.firstOrderOwner, confirmedToTranslate: false, 
+                                                             lastOrderOwner: '', markedToTranslate: false };
+            Orders.update( { _id: _pOrder._id }, { $set: { creation_user: _pOrder.translateInfo.firstOrderOwner,
+                                                           modification_user: this._user,
+                                                           translateInfo: _lOrderTranslateInfo 
+                                                         } 
+                                                 }
+                         );
+        }
+    }
+
+    /**
+     * When user wants see payment detail, this function open dialog with orders information
+     */
+    open(){
+        this._dialogRef = this._dialog.open( ColombiaPaymentDetailComponent, {
+            disableClose : true,
+            width: '50%',
+            height: '85%'
+        });
+        this._dialogRef.componentInstance.currId = this.currId;
+        this._dialogRef.componentInstance.currencyCode = this._currencyCode;
+        this._dialogRef.afterClosed().subscribe( result => {
+            this._dialogRef = null;
+        });
+    }
+
+    /**
+     * Allow user add tip in total payment
+     * @param {any} _event 
+     */
+    allowTip( _event:any ):void{
+        if( _event.checked ){
+            this._totalToPayment = this._totalToPayment + this._tipTotal;
+        } else {
+            this._totalToPayment = this._totalToPayment - this._tipTotal;
+        }
+    }
+
+    /**
+     * Allow user add other tip in total payment
+     * @param {any} _event
+     */
+    allowOtherTip( _event:any ):void{
+        if( _event.checked ){
+            this._otherTipAllowed = false;
+        } else {
+            this._otherTipAllowed = true;            
+            this._totalToPayment = this._totalToPayment - this._otherTip;
+            this._otherTip = 0;
+            this._oldOtherTip = 0;
+        }
+    }
+
+    /**
+     * Sum Other tip in total payment
+     * @param {any} _pEvent 
+     */
+    sumOtherTip( _pEvent: any ):void{
+        if( _pEvent !== null ){ 
+            this._totalToPayment = this._totalToPayment - this._oldOtherTip;
+            this._totalToPayment = this._totalToPayment + this._otherTip;
+            this._oldOtherTip = this._otherTip;
+        } else {
+            this._totalToPayment = this._totalToPayment - this._otherTip;
+            this._otherTip = 0;
+            this._oldOtherTip = 0;
+        }
     }
 
     /**
