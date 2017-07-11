9334524fa33c45ee5fbe42c28c6c203d1bce906b
Leonardo González <clgrhc@gmail.com>
2017-07-11 18:54:55 -0500

    finished form to payu payment

--- qmo_web/client/imports/app/web/payment/payu-payment-form/payu-payment-form.component.ts
+++ qmo_web/client/imports/app/web/payment/payu-payment-form/payu-payment-form.component.ts
@@ -8,6 +8,10 @@ import { CustomValidators } from '../../../../../../both/shared-components/valid
 
 import { CcPaymentMethods } from '../../../../../../both/collections/payment/cc-payment-methods.collection';
 import { CcPaymentMethod } from '../../../../../../both/models/payment/cc-payment-method.model';
+import { Countries } from '../../../../../../both/collections/settings/country.collection';
+import { Country } from '../../../../../../both/models/settings/country.model';
+import { City } from '../../../../../../both/models/settings/city.model';
+import { Cities } from '../../../../../../both/collections/settings/city.collection';
 
 import { PayuPaymenteService } from '../payu-payment-service/payu-payment.service';
 
@@ -26,8 +30,14 @@ export class PayuPaymentFormComponent implements OnInit, OnDestroy {
     private _selectedPaymentMethod: string;
     private _selectedCardMonth: string;
     private _selectedCardYear: string;
+    private _selectedCountry: string;
+    private _selectedCity: string = "";
     private _cCPaymentMethodSub: Subscription;
     private _cCPaymentMethods: Observable<CcPaymentMethod[]>;
+    private _countrySub: Subscription;
+    private _countries: Observable<Country[]>;
+    private _citySub: Subscription;
+    private _cities: Observable<City[]>;
     private _currentDate: Date;
     private _currentYear: number;
     private _yearsArray: any[];
@@ -60,7 +70,14 @@ export class PayuPaymentFormComponent implements OnInit, OnDestroy {
             cardNumber: new FormControl('', [Validators.required, Validators.minLength(13), Validators.maxLength(20), CustomValidators.numericValidator]),
             expirationMonth: new FormControl('', [Validators.required, Validators.minLength(2), Validators.maxLength(2)]),
             expirationYear: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(4)]),
-            securityCode: new FormControl('', [Validators.required, Validators.minLength(1), Validators.maxLength(4)])
+            securityCode: new FormControl('', [Validators.required, Validators.minLength(1), Validators.maxLength(4), CustomValidators.numericValidator]),
+            email: new FormControl('', [Validators.required, Validators.minLength(6), Validators.maxLength(255), CustomValidators.emailValidator]),
+            fullName: new FormControl('', [Validators.required, Validators.minLength(1), Validators.maxLength(150)]),
+            dniNumber: new FormControl('', [Validators.required, Validators.minLength(1), Validators.maxLength(20)]),
+            country: new FormControl('', [Validators.required]),
+            city: new FormControl('', [Validators.required]),
+            streetOne: new FormControl('', [Validators.required, Validators.minLength(1), Validators.maxLength(100)]),
+            contactPhone: new FormControl('',[Validators.required, Validators.minLength(1), Validators.maxLength(20)])
         });
 
         this._cCPaymentMethodSub = MeteorObservable.subscribe('getCcPaymentMethods').subscribe(() => {
@@ -69,10 +86,24 @@ export class PayuPaymentFormComponent implements OnInit, OnDestroy {
             });
         });
 
-        this._monthsArray = [{ value: '01', viewValue: '01' }, { value: '02', viewValue: '02' }, { value: '03', viewValue: '03' }, 
-                             { value: '04', viewValue: '04' }, { value: '05', viewValue: '05' }, { value: '06', viewValue: '06' }, 
-                             { value: '07', viewValue: '07' }, { value: '08', viewValue: '08' }, { value: '09', viewValue: '09' }, 
-                             { value: '10', viewValue: '10' }, { value: '11', viewValue: '11' }, { value: '12', viewValue: '12' }];
+        this._countrySub = MeteorObservable.subscribe('countries').subscribe(() => {
+            this._ngZone.run(() => {
+                this._countries = Countries.find({}).zone();
+            });
+        });
+
+        this._citySub = MeteorObservable.subscribe('cities').subscribe(() => {
+            this._ngZone.run(() => {
+                this._cities = Cities.find({country: ''}).zone();
+            });
+        });
+
+
+
+        this._monthsArray = [{ value: '01', viewValue: '01' }, { value: '02', viewValue: '02' }, { value: '03', viewValue: '03' },
+        { value: '04', viewValue: '04' }, { value: '05', viewValue: '05' }, { value: '06', viewValue: '06' },
+        { value: '07', viewValue: '07' }, { value: '08', viewValue: '08' }, { value: '09', viewValue: '09' },
+        { value: '10', viewValue: '10' }, { value: '11', viewValue: '11' }, { value: '12', viewValue: '12' }];
 
         this._currentDate = new Date();
         this._currentYear = this._currentDate.getFullYear();
@@ -83,7 +114,10 @@ export class PayuPaymentFormComponent implements OnInit, OnDestroy {
             let auxYear = { value: this._currentYear + i, viewValue: this._currentYear + i };
             this._yearsArray.push(auxYear);
         }
+    }
 
+    changeCountry(countryId: string){
+        this._cities = Cities.find({ country: countryId }).zone();
     }
 
     getCode() {
