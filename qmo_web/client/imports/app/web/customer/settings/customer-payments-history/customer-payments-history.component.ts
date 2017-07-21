6568d06cf1550cbc68ce6f08270496e5c5d6cb38
Fernando Arévalo <fernando.arevalo9311@gmail.com>
2017-07-21 18:57:39 -0500

    Implement Invoice pdf

--- qmo_web/client/imports/app/web/customer/settings/customer-payments-history/customer-payments-history.component.ts
+++ qmo_web/client/imports/app/web/customer/settings/customer-payments-history/customer-payments-history.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, OnDestroy, NgZone, Input } from '@angular/core';
 import { MeteorObservable } from "meteor-rxjs";
+import { TranslateService } from 'ng2-translate';
 import { Subscription } from 'rxjs';
 import { Invoice } from '../../../../../../../both/models/restaurant/invoice.model';
 import { Invoices } from '../../../../../../../both/collections/restaurant/invoice.collection';
@@ -31,7 +32,8 @@ export class CustomerPaymentsHistoryComponent implements OnInit, OnDestroy {
      * PaymentsHistoryComponent component
      * @param _ngZone 
      */ 
-    constructor(private _ngZone: NgZone){
+    constructor(private _ngZone: NgZone,
+                public _translate: TranslateService){
     }
 
     /**
@@ -54,11 +56,62 @@ export class CustomerPaymentsHistoryComponent implements OnInit, OnDestroy {
     }
 
     invoiceGenerate( _pInvoice : Invoice){
-        let qr_pdf = new jsPDF("portrait", "mm", "a7");
-        qr_pdf.setFontSize(8);
-        qr_pdf.text(_pInvoice.financial_information.business_name, 10, 40)
-        qr_pdf.output('dataurlnewwindow');
+        let pdf = new jsPDF("portrait", "pt", "a7");
+        pdf.setFontSize(8);
+        let widthText   : number = 180;
+        let x           : number = 105;
+        let y           : number = 50;
+        let alignCenter : string = 'center';
+        let alignRight : string  = 'right';
 
+        let splitBusinessName = pdf.splitTextToSize(_pInvoice.financial_information.business_name, widthText  );
+        let splitNit          = pdf.splitTextToSize(_pInvoice.financial_information.nit, widthText);
+        let splitAddress      = pdf.splitTextToSize(_pInvoice.financial_information.address, widthText);
+
+        let despcriptionTitle = this.itemNameTraduction('DESCRIPTION');
+        let quantTitle        = this.itemNameTraduction('PAYMENTS.COLOMBIA.QUANT');
+        let valueTitle        = this.itemNameTraduction('VALUE');
+
+        pdf.text( splitBusinessName, x, y, alignCenter );
+        y = this.calculateY(y, 20);
+        pdf.text( this.itemNameTraduction('FINANCIAL_INFO.COLOMBIA.NIT_LABEL') + ' ' + splitNit, x, y, alignCenter );
+        y = this.calculateY(y, 10);
+        pdf.text( splitAddress, x, y, alignCenter );
+        
+        y = this.calculateY(y, 30);
+        pdf.text( despcriptionTitle, 10, y );
+        pdf.text( quantTitle, 140, y );
+        pdf.text( valueTitle, 200, y, 'right' );
+        
+        y = this.calculateY(y, 10);
+        _pInvoice.items.forEach( (item) => {
+            y = this.calculateY(y, 10);
+            pdf.text( item.item_name, 10, y.toString() );
+            pdf.text( item.quantity, 140, y.toString() );
+            //pdf.text( item.price, 200, y.toString(), 'right' );
+        });
+
+        
+        
+
+        pdf.output('dataurlnewwindow');
+    }
+
+    calculateY( _pY : number, _pAdd : number) : number{
+        _pY = _pY + _pAdd;
+        return _pY;
+    }
+
+    /**
+   * This function allow translate strings
+   * @param {string} _itemName 
+   */
+    itemNameTraduction(_itemName: string): string {
+        var wordTraduced: string;
+        this._translate.get(_itemName).subscribe((res: string) => {
+        wordTraduced = res;
+        });
+        return wordTraduced;
     }
 
     /**
