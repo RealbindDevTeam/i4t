a687b95d2f1c006206e113e76b879ec45883817d
Leonardo González <clgrhc@gmail.com>
2017-08-05 15:35:51 -0500

    implemented inheritance for signup and signin

--- qmo_web/client/imports/app/web/auth/signin.web.component.ts
+++ qmo_web/client/imports/app/web/auth/signin.web.component.ts
@@ -7,7 +7,7 @@ import { TranslateService } from 'ng2-translate';
 import { UserLanguageService } from '../../shared/services/user-language.service';
 import { RecoverWebComponent } from './recover-password/recover.web.component';
 import { UserDetails } from '../../../../../both/collections/auth/user-detail.collection';
-import { AuthClass} from './auth.class';
+import { AuthClass } from './auth.class';
 
 import template from './signin.web.component.html';
 import style from './auth.component.scss';
@@ -18,30 +18,27 @@ import style from './auth.component.scss';
     styles: [style]
 })
 
-export class SigninWebComponent implements OnInit{
+export class SigninWebComponent extends AuthClass implements OnInit {
 
     private signinForm: FormGroup;
-    private mdDialogRef: MdDialogRef<any>;
+    private mdDialogRef2: MdDialogRef<any>;
     private error: string;
+
     /**
      * SigninWebComponent Constructor
      * @param {Router} router 
      * @param {NgZone} zone 
      * @param {TranslateService} translate 
-     * @param {ViewContainerRef} viewContainerRef 
      * @param {MdDialog} mdDialog 
      * @param {UserLanguageService} _userLanguageService 
      */
-    constructor(private router: Router,
+    constructor(protected router: Router,
         protected zone: NgZone,
         protected translate: TranslateService,
-        public viewContainerRef: ViewContainerRef,
-        public mdDialog: MdDialog,
-        public _userLanguageService: UserLanguageService) {
+        protected _userLanguageService: UserLanguageService,
+        protected _mdDialog: MdDialog) {
 
-        translate.use(this._userLanguageService.getNavigationLanguage());
-        translate.setDefaultLang('en');
-
+        super(router, zone, translate, _userLanguageService, _mdDialog);
         if (!Meteor.user()) {
             Meteor.logout();
         }
@@ -63,9 +60,15 @@ export class SigninWebComponent implements OnInit{
         if (respLogin) {
             if (this.signinForm.valid) {
                 Meteor.loginWithPassword(this.signinForm.value.email, this.signinForm.value.password, (err) => {
+                    let confirmMsg: string;
                     this.zone.run(() => {
                         if (err) {
-                            this.error = err;
+                            if (err.reason === 'User not found' || err.reason === 'Incorrect password') {
+                                confirmMsg = 'SIGNIN.USER_PASS_INCORRECT';
+                            } else {
+                                confirmMsg = 'SIGNIN.ERROR';
+                            }
+                            this.openDialog(this.titleMsg, '', confirmMsg, '', this.btnAcceptLbl, false);
                         } else {
                             MeteorObservable.call('getRole').subscribe((role) => {
                                 switch (role) {
@@ -91,7 +94,8 @@ export class SigninWebComponent implements OnInit{
                                     }
                                 }
                             }, (error) => {
-                                this.error = err;
+                                confirmMsg = 'SIGNIN.ERROR';
+                                this.openDialog(this.titleMsg, '', confirmMsg, '', this.btnAcceptLbl, false);
                             });
                         }
                     });
@@ -104,121 +108,14 @@ export class SigninWebComponent implements OnInit{
     }
 
     /**
-     * This function login the user with facebook social network
-     */
-    loginWithFacebook() {
-        let respLogin = this.devicesValidate();
-        if (respLogin) {
-            Meteor.loginWithFacebook({ requestPermissions: ['public_profile', 'email'] }, (err) => {
-
-                this.zone.run(() => {
-                    if (err) {
-                        this.error = err;
-                    } else {
-                        this.insertUserDetail();
-                    }
-                });
-            });
-        }
-        else {
-            this.router.navigate(['go-to-store']);
-        }
-    }
-
-    /**
-     * This function login the user with twitter social network
-     */
-    loginWithTwiteer() {
-        let respLogin = this.devicesValidate();
-        if (respLogin) {
-            Meteor.loginWithTwitter({ requestPermissions: [] }, (err) => {
-                this.zone.run(() => {
-                    if (err) {
-                        this.error = err;
-                    } else {
-                        this.insertUserDetail();
-                    }
-                });
-            });
-        }
-        else {
-            this.router.navigate(['go-to-store']);
-        }
-    }
-
-    /**
-     * This function login the user with google
-     */
-    loginWithGoogle() {
-        let respLogin = this.devicesValidate();
-        if (respLogin) {
-            Meteor.loginWithGoogle({ requestPermissions: [] }, (err) => {
-                this.zone.run(() => {
-                    if (err) {
-                        this.error = err;
-                    } else {
-                        this.insertUserDetail();
-                    }
-                });
-            });
-        }
-        else {
-            this.router.navigate(['go-to-store']);
-        }
-    }
-
-    /**
-     * This function insert de user data 
-     */
-    insertUserDetail() {
-        MeteorObservable.call('getDetailsCount').subscribe((count) => {
-
-            if (count === 0) {
-                UserDetails.insert({
-                    user_id: Meteor.userId(),
-                    role_id: '400',
-                    is_active: true,
-                    restaurant_work: '',
-                    penalties: [],
-                    current_restaurant: '',
-                    current_table: ''
-                });
-            }
-
-            //this.router.navigate(['app/orders']);
-            this.router.navigate(['app/orders']);
-        }, (error) => {
-            this.error;
-        });
-    }
-
-    /**
-     * This function validate de devices and return boolean
-     * @return {boolean}
-     */
-    devicesValidate(): boolean {
-        if (navigator.userAgent.match(/Android/i)
-            || navigator.userAgent.match(/webOS/i)
-            || navigator.userAgent.match(/iPhone/i)
-            || navigator.userAgent.match(/iPad/i)
-            || navigator.userAgent.match(/iPod/i)
-            || navigator.userAgent.match(/BlackBerry/i)
-            || navigator.userAgent.match(/Windows Phone/i)) {
-            return false;
-        } else {
-            return true
-        }
-    }
-
-    /**
      * This function opens the forgot password dialog
      */
     openDialogForgotPassword() {
-        this.mdDialogRef = this.mdDialog.open(RecoverWebComponent, {
+        this.mdDialogRef2 = this._mdDialog.open(RecoverWebComponent, {
             disableClose: true,
         });
-        this.mdDialogRef.afterClosed().subscribe(result => {
-            this.mdDialogRef = null;
+        this.mdDialogRef2.afterClosed().subscribe(result => {
+            this.mdDialogRef2 = null;
         });
     }
 }
