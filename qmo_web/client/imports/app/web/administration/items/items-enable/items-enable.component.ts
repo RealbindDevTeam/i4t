e709c6382b453249c77ae97acb5cbba23d431937
Leonardo González <clgrhc@gmail.com>
2017-08-18 18:55:43 -0500

    started dialog for enable items by admin

--- qmo_web/client/imports/app/web/administration/items/items-enable/items-enable.component.ts
+++ qmo_web/client/imports/app/web/administration/items/items-enable/items-enable.component.ts
@@ -13,14 +13,14 @@ import style from '../item.component.scss';
 @Component({
     selector: 'item-enable',
     template,
-    styles: [ style ]
+    styles: [style]
 })
 export class ItemEnableComponent implements OnInit, OnDestroy {
-    
-    private _itemsSub           : Subscription;
-    private _itemImagesSub      : Subscription;
-    private _items              : Observable<Item[]>;
-    private _itemsFilter        : Item[] = [];
+
+    private _itemsSub: Subscription;
+    private _itemImagesSub: Subscription;
+    private _items: Observable<Item[]>;
+    private _itemsFilter: Item[] = [];
 
     /**
      * ItemEnableComponent Constructor
@@ -28,49 +28,35 @@ export class ItemEnableComponent implements OnInit, OnDestroy {
      * @param {NgZone} _ngZone
      * @param {UserLanguageService} _userLanguageService
      */
-    constructor( private _translate: TranslateService, 
-                 private _ngZone: NgZone,
-                 private _userLanguageService: UserLanguageService ){
-        _translate.use( this._userLanguageService.getLanguage( Meteor.user() ) );
-        _translate.setDefaultLang( 'en' );
+    constructor(private _translate: TranslateService,
+        private _ngZone: NgZone,
+        private _userLanguageService: UserLanguageService) {
+        _translate.use(this._userLanguageService.getLanguage(Meteor.user()));
+        _translate.setDefaultLang('en');
     }
 
     /**
      * ngOnInit implementation
      */
-    ngOnInit(){
-        this._itemsSub = MeteorObservable.subscribe( 'items', Meteor.userId() ).subscribe( () => {
-            this._ngZone.run( () => {
-                this._items = Items.find( { } ).zone();
-                this._itemsFilter = Items.collection.find( { } ).fetch();
+    ngOnInit() {
+        this._itemsSub = MeteorObservable.subscribe('items', Meteor.userId()).subscribe(() => {
+            this._ngZone.run(() => {
+                this._items = Items.find({}).zone();
+                this._itemsFilter = Items.collection.find({}).fetch();
             });
         });
-        this._itemImagesSub = MeteorObservable.subscribe( 'itemImages', Meteor.userId() ).subscribe();
-    }
-
-    /**
-     * Update item available flag
-     * @param {Item} _item 
-     */
-    updateAvailableFlag( _item:Item ):void{
-        Items.update( _item._id, {
-            $set: {
-                //isAvailable: !_item.isAvailable,
-                modification_date: new Date(),
-                modification_user: Meteor.userId()
-            }
-        });
+        this._itemImagesSub = MeteorObservable.subscribe('itemImages', Meteor.userId()).subscribe();
     }
 
     /**
      * Return item image
      * @param {string} _itemId
      */
-    getItemImage( _itemId:string ):string{
-        let _lItemImage: ItemImage = ItemImages.findOne( { itemId: _itemId } );
-        if( _lItemImage ){
+    getItemImage(_itemId: string): string {
+        let _lItemImage: ItemImage = ItemImages.findOne({ itemId: _itemId });
+        if (_lItemImage) {
             return _lItemImage.url;
-        } else{
+        } else {
             return '/images/default-plate.png';
         }
     }
@@ -78,7 +64,7 @@ export class ItemEnableComponent implements OnInit, OnDestroy {
     /**
      * ngOnDestroy implementation
      */
-    ngOnDestroy(){
+    ngOnDestroy() {
         this._itemsSub.unsubscribe();
         this._itemImagesSub.unsubscribe();
     }
