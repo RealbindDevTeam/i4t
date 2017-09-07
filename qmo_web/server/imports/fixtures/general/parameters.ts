4c3730bb77bd7989d1e4765cd0341aac1b778683
Leonardo González <clgrhc@gmail.com>
2017-09-07 07:34:02 -0500

    finished admin customer invoice

--- qmo_web/server/imports/fixtures/general/parameters.ts
+++ qmo_web/server/imports/fixtures/general/parameters.ts
@@ -13,10 +13,15 @@ export function loadParameters() {
             { _id: '700', name: 'payu_script_img_tag', value: 'https://maf.pagosonline.net/ws/fp/clear.png?id=', description: 'url for security script for payu form in <img> tag' },
             { _id: '800', name: 'payu_script_script_tag', value: 'https://maf.pagosonline.net/ws/fp/check.js?id=', description: 'url for security script for payu form in <script> tag' },
             { _id: '900', name: 'payu_script_object_tag', value: 'https://maf.pagosonline.net/ws/fp/fp.swf?id=', description: 'url for security script for payu form in <object> tag' },
-            { _id: '1000', name: 'payu_payments_url', value: 'https://sandbox.api.payulatam.com/payments-api/4.0/service.cgi', description: 'url for connect payu payments API'},
-            { _id: '2000', name: 'payu_reports_url', value: 'https://sandbox.api.payulatam.com/reports-api/4.0/service.cgi', description: 'url for connect payu reports API'},
-            { _id: '3000', name: 'ip_public_service_url', value: 'https://api.ipify.org?format=json', description: 'url for retrieve the client public ip'},
-            { _id: '4000', name: 'payu_pay_info_url', value: 'http://192.168.0.10:9000/api/getCusPayInfo', description: 'url for retrieve credentials for payu payment'}
+            { _id: '1000', name: 'payu_payments_url', value: 'https://sandbox.api.payulatam.com/payments-api/4.0/service.cgi', description: 'url for connect payu payments API' },
+            { _id: '2000', name: 'payu_reports_url', value: 'https://sandbox.api.payulatam.com/reports-api/4.0/service.cgi', description: 'url for connect payu reports API' },
+            { _id: '3000', name: 'ip_public_service_url', value: 'https://api.ipify.org?format=json', description: 'url for retrieve the client public ip' },
+            { _id: '4000', name: 'payu_pay_info_url', value: 'http://192.168.0.10:9000/api/getCusPayInfo', description: 'url for retrieve credentials for payu payment' },
+            { _id: '1100', name: 'company_name', value: 'Realbind S.A.S', description: 'Realbind company name for invoice' },
+            { _id: '1200', name: 'company_address', value: 'Cra 6 # 58-43 Of 201', description: 'Realbind company address' },
+            { _id: '1300', name: 'company_country', value: 'Colombia', description: 'Realbind country location' },
+            { _id: '1400', name: 'company_city', value: 'Bogotá', description: 'Realbind city location' },
+            { _id: '1500', name: 'company_nit', value: 'NIT: 78301921-1', description: 'Realbind NIT' }
         ];
 
         parameters.forEach((parameter: Parameter) => Parameters.insert(parameter));
