42937d6a8b210d6d079fe6ae0c160e1b07148e31
Leonardo González <clgrhc@gmail.com>
2017-09-20 19:00:52 -0500

    started edition for restaurant-exit

--- i4t_mobile/src/pages/customer/options/restaurant-exit/restaurant-exit.ts
+++ i4t_mobile/src/pages/customer/options/restaurant-exit/restaurant-exit.ts
@@ -5,7 +5,11 @@ import { TranslateService } from '@ngx-translate/core';
 import { Subscription } from 'rxjs';
 import { UserLanguageService } from 'qmo_web/client/imports/app/shared/services/user-language.service';
 import { Tables } from 'qmo_web/both/collections/restaurant/table.collection';
-import { Restaurants, RestaurantImageThumbs } from 'qmo_web/both/collections/restaurant/restaurant.collection';
+import { Restaurants, RestaurantImages, RestaurantImageThumbs } from 'qmo_web/both/collections/restaurant/restaurant.collection';
+import { UserDetails } from 'qmo_web/both/collections/auth/user-detail.collection';
+import { Orders } from 'qmo_web/both/collections/restaurant/order.collection';
+import { Payments } from 'qmo_web/both/collections/restaurant/payment.collection';
+import { WaiterCallDetails } from 'qmo_web/both/collections/restaurant/waiter-call-detail.collection';
 
 @Component({
     selector: 'restaurant-exit',
@@ -20,9 +24,15 @@ export class RestaurantExitPage implements OnInit, OnDestroy {
     private _restaurantSub: Subscription;
     private _tablesSub: Subscription;
     private _restaurantThumbSub: Subscription;
+    private _restaurantImagesSub: Subscription;
+    private _userDetailSub: Subscription;
+    private _ordersSub: Subscription;
 
     private _restaurant: any;
     private _table: any;
+    private _orders: any;
+
+    private _showWaiterCard: boolean = false;
 
     constructor(public _navCtrl: NavController,
         public _navParams: NavParams,
@@ -59,21 +69,89 @@ export class RestaurantExitPage implements OnInit, OnDestroy {
             });
 
             this._restaurantThumbSub = MeteorObservable.subscribe('restaurantImageThumbsByUserId', Meteor.userId()).subscribe();
+            this._restaurantImagesSub = MeteorObservable.subscribe('restaurantImageByUserId', Meteor.userId()).subscribe();
+            this._userDetailSub = MeteorObservable.subscribe('getUserDetailsByUser', Meteor.userId()).subscribe();
+
+            this._ordersSub = MeteorObservable.subscribe('getOrdersByUserId', Meteor.userId(), ['ORDER_STATUS.REGISTERED', 'ORDER_STATUS.IN_PROCESS', 'ORDER_STATUS.PREPARED',
+                'ORDER_STATUS.DELIVERED', 'ORDER_STATUS.PENDING_CONFIRM']).subscribe(() => {
+                    this._ngZone.run(() => {
+                        this._orders = Orders.find({}).zone();
+                        this._orders.subscribe(() => { this.validateOrdersMarkedToCancel(); });
+                    });
+                });
         }
     }
 
-    getRestaurantThumb(_id: string): string {
-        let _imageThumb;
-        _imageThumb = RestaurantImageThumbs.find().fetch().filter((i) => i.restaurantId === _id)[0];
-        if (_imageThumb) {
-            return _imageThumb.url;
+    getRestaurantImage(_id: string): string {
+        let _image;
+        _image = RestaurantImages.find().fetch().filter((i) => i.restaurantId === _id)[0];
+        if (_image) {
+            return _image.url;
         } else {
             return 'assets/img/default-restaurant.png';
         }
     }
 
     exitRestaurant() {
-        //TODO
+        let userDetail = UserDetails.findOne({ user_id: Meteor.userId() })._id;
+        console.log(userDetail);
+
+        let _lOrdersRegisteredStatus: number = Orders.collection.find({ creation_user: Meteor.userId(), restaurantId: this._res_code, tableId: this._table_code, status: 'ORDER_STATUS.REGISTERED' }).count();
+        let _lOrdersInProcessStatus: number = Orders.collection.find({ creation_user: Meteor.userId(), restaurantId: this._res_code, tableId: this._table_code, status: 'ORDER_STATUS.IN_PROCESS' }).count();
+        let _lOrdersPreparedStatus: number = Orders.collection.find({ creation_user: Meteor.userId(), restaurantId: this._res_code, tableId: this._table_code, status: 'ORDER_STATUS.PREPARED' }).count();
+        let _lOrdersDeliveredStatus: number = Orders.collection.find({ creation_user: Meteor.userId(), restaurantId: this._res_code, tableId: this._table_code, status: 'ORDER_STATUS.DELIVERED', toPay: false }).count();
+        let _lOrdersToConfirm: number = Orders.collection.find({ restaurantId: this._res_code, tableId: this._table_code, 'translateInfo.firstOrderOwner': Meteor.userId(), 'translateInfo.markedToTranslate': true, status: 'ORDER_STATUS.PENDING_CONFIRM', toPay: false }).count();
+        let _lOrdersWithPendingConfirmation: number = Orders.collection.find({ restaurantId: this._res_code, tableId: this._table_code, 'translateInfo.lastOrderOwner': Meteor.userId(), 'translateInfo.markedToTranslate': true, status: 'ORDER_STATUS.PENDING_CONFIRM', toPay: false }).count();
+        let _lUserWaiterCallsCount: number = WaiterCallDetails.collection.find({ restaurant_id: this._res_code, table_id: this._table_code, type: 'CALL_OF_CUSTOMER', user_id: Meteor.userId(), status: 'completed' }).count();
+        let _lUserPaymentsCount: number = Payments.collection.find({ creation_user: Meteor.userId(), restaurantId: this._res_code, tableId: this._table_code, status: 'PAYMENT.NO_PAID', received: false }).count();
+
+        if (_lOrdersRegisteredStatus === 0 && _lOrdersInProcessStatus === 0 && _lOrdersPreparedStatus === 0
+            && _lOrdersDeliveredStatus === 0 && _lOrdersToConfirm === 0 && _lOrdersWithPendingConfirmation === 0
+            && _lUserWaiterCallsCount === 0 && _lUserPaymentsCount === 0) {
+
+            let confirm = this._alertCtrl.create({
+                title: this.itemNameTraduction('EXIT_TABLE.RESTAURANT_EXIT'),
+                message: this.itemNameTraduction('EXIT_TABLE.RESTAURANT_EXIT_CONFIRM'),
+                buttons: [
+                    {
+                        text: this.itemNameTraduction('NO'),
+                        handler: () => {
+                            console.log('Disagree clicked');
+                        }
+                    },
+                    {
+                        text: this.itemNameTraduction( 'YES' ),
+                        handler: () => {
+                          console.log('Agree clicked');
+                        }
+                    }
+                ]
+            });
+            confirm.present();
+
+        } else {
+
+        }
+    }
+
+    cancelWaiterCall() {
+        console.log('cancelWaiterCall');
+    }
+
+    itemNameTraduction(itemName: string): string {
+        var wordTraduced: string;
+        this._translate.get(itemName).subscribe((res: string) => {
+            wordTraduced = res;
+        });
+        return wordTraduced;
+    }
+
+    /**
+     * Validate orders marked to cancel
+     */
+    validateOrdersMarkedToCancel(): void {
+        let _lOrdersToCancelCount: number = Orders.collection.find({ creation_user: Meteor.userId(), status: { $in: ['ORDER_STATUS.IN_PROCESS', 'ORDER_STATUS.PREPARED'] }, markedToCancel: true }).count();
+        _lOrdersToCancelCount > 0 ? this._showWaiterCard = true : this._showWaiterCard = false;
     }
 
     ngOnDestroy() {
@@ -91,6 +169,9 @@ export class RestaurantExitPage implements OnInit, OnDestroy {
         if (this._restaurantSub) { this._restaurantSub.unsubscribe(); }
         if (this._tablesSub) { this._tablesSub.unsubscribe(); }
         if (this._restaurantThumbSub) { this._restaurantThumbSub.unsubscribe(); }
+        if (this._restaurantImagesSub) { this._restaurantImagesSub.unsubscribe(); }
+        if (this._userDetailSub) { this._userDetailSub.unsubscribe(); }
+        if (this._ordersSub) { this._ordersSub.unsubscribe(); }
     }
 }
 
