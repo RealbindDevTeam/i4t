c87da358e31ff9202f8e1d8a769639bbea2442f6
Fernando Arévalo <fernando.arevalo9311@gmail.com>
2017-09-21 19:04:42 -0500

    change implemented to error on the process jobs

--- qmo_web/both/methods/restaurant/waiter-queue/waiter-queue.methods.ts
+++ qmo_web/both/methods/restaurant/waiter-queue/waiter-queue.methods.ts
@@ -16,7 +16,7 @@ if (Meteor.isServer) {
 
   Meteor.methods({
     /**
-     * This Meteor Method add job in the Waiter call queue
+     * This Meteor Method add a job in the Waiter call queue
      * @param {boolean} _priorityHigh
      * @param {any} _data
      */
@@ -30,10 +30,9 @@ if (Meteor.isServer) {
         'waiterCall',
         { data : '' }
       );
-      job
-        .priority(priority)
-        .delay(delay)
-        .save();
+      job.priority(priority)
+         .delay(delay)
+         .save();
 
       if (_priorityHigh) {
         priority = 'critical', delay = 10000;
@@ -59,49 +58,64 @@ if (Meteor.isServer) {
     },
 
     processJobs : function(job, callback, queueName){
-      var data_detail = WaiterCallDetails.collection.findOne({ job_id : job._doc._id });
-      var restaurant = Restaurants.collection.findOne({ _id : data_detail.restaurant_id });
-      var usr_id_enabled : UserDetail = Meteor.call('validateWaiterEnabled', data_detail.restaurant_id, restaurant.max_jobs, data_detail.table_id);
-      if( usr_id_enabled ){
-        Job.getJob(queueName, job._doc._id, function (err, job) {
-          job.done(function (err, result){});
-          //Storage of turns the restaurants by date
-          var toDate = new Date().toLocaleDateString();
-          RestaurantTurns.update({restaurant_id : data_detail.restaurant_id , creation_date : { $gte : new Date(toDate)}}, 
-            {$set : { last_waiter_id : usr_id_enabled.user_id , modification_user: 'SYSTEM', modification_date: new Date(),  }
+      var data_detail = WaiterCallDetails.findOne({ job_id : job._doc._id });
+      if(data_detail){
+        var restaurant = Restaurants.findOne({ _id : data_detail.restaurant_id });
+        var usr_id_enabled : UserDetail = Meteor.call('validateWaiterEnabled', data_detail.restaurant_id, restaurant.max_jobs, data_detail.table_id);
+        if( usr_id_enabled ){
+          Job.getJob(queueName, job._doc._id, function (err, job) {
+            job.done(function (err, result){});
+            //Storage of turns the restaurants by date
+            var toDate = new Date().toLocaleDateString();
+            RestaurantTurns.update({restaurant_id : data_detail.restaurant_id , creation_date : { $gte : new Date(toDate)}}, 
+              {$set : { last_waiter_id : usr_id_enabled.user_id , modification_user: 'SYSTEM', modification_date: new Date(),  }
+            });
+            //Waiter call detail update in completed state
+            WaiterCallDetails.update({ job_id : job._doc._id },
+              { $set : { "waiter_id" : usr_id_enabled.user_id, "status" : "completed"}
+            });
           });
-          //Waiter call detail update in completed state
-          WaiterCallDetails.update({ job_id : job._doc._id },
-            { $set : { "waiter_id" : usr_id_enabled.user_id, "status" : "completed"}
-          });
-        });
-
-        //Waiter update of current jobs and state
-        let usr_jobs : number = usr_id_enabled.jobs + 1 ;
-        if ( usr_jobs < restaurant.max_jobs ) {
-            UserDetails.update({user_id : usr_id_enabled.user_id} , { $set : { "jobs" : usr_jobs } });
-        } else if ( usr_jobs == restaurant.max_jobs ){
-            UserDetails.update({user_id : usr_id_enabled.user_id} , { $set : { "enabled" : false, "jobs" : usr_jobs } });
+          //Waiter update of current jobs and state
+          let usr_jobs : number = usr_id_enabled.jobs + 1 ;
+          if ( usr_jobs < restaurant.max_jobs ) {
+              UserDetails.update({user_id : usr_id_enabled.user_id} , { $set : { "jobs" : usr_jobs } });
+          } else if ( usr_jobs == restaurant.max_jobs ){
+              UserDetails.update({user_id : usr_id_enabled.user_id} , { $set : { "enabled" : false, "jobs" : usr_jobs } });
+          }
+        } else {
+          Meteor.call('jobRemove', queueName, job._doc._id, data_detail, usr_id_enabled);
         }
       } else {
-          Job.getJob(queueName, job._doc._id, function (err, job) {
-              job.cancel();
-              var data  : any = {
-                  job_id : job._doc._id,
-                  restaurants : data_detail.restaurant_id,
-                  tables : data_detail.table_id,
-                  user : data_detail.user_id,
-                  waiter_id : usr_id_enabled,
-                  status : 'waiting'
-              };
-              job.remove(function (err, result) {
-                  if (result) {
-                      Meteor.call('waiterCall', queueName,true, data);
-                  }
-              });
-          });
+        Meteor.call('jobRemove', queueName, job._doc._id, data_detail, usr_id_enabled);
+        throw new Meteor.Error('200 (processJobs) - WaiterCallDetails is undefined');
       }
       callback();
+    },
+
+    /**
+     * Job remove
+     * @param pQueueName 
+     * @param pJobId 
+     * @param pDataDetail 
+     * @param pEnabled 
+     */
+    jobRemove(pQueueName, pJobId, pDataDetail, pEnabled){
+      Job.getJob(pQueueName, pJobId, function (err, job) {
+        job.cancel();
+        var data  : any = {
+            job_id : job._doc._id,
+            restaurants : pDataDetail.restaurant_id,
+            tables : pDataDetail.table_id,
+            user : pDataDetail.user_id,
+            waiter_id : pEnabled,
+            status : 'waiting'
+        };
+        job.remove(function (err, result) {
+            if (result) {
+                Meteor.call('waiterCall', pQueueName, true, data);
+            }
+        });
+      });
     },
 
     /**
@@ -111,7 +125,7 @@ if (Meteor.isServer) {
     turnCreate ( _data : any ) : number {
       var newTurn : number = 1;
       var toDate = new Date().toLocaleDateString();
-      var restaurantTurn : RestaurantTurn = RestaurantTurns.collection.findOne({
+      var restaurantTurn : RestaurantTurn = RestaurantTurns.findOne({
           restaurant_id : _data.restaurants,
           creation_date : { $gte : new Date(toDate)}
       });
@@ -146,7 +160,7 @@ if (Meteor.isServer) {
             { $set : { "status" : "closed", modification_user : _waiter_id, modification_date : new Date() }
           });
           
-          let waiterDetail = WaiterCallDetails.collection.findOne({job_id : _jobDetail.job_id});
+          let waiterDetail = WaiterCallDetails.findOne({job_id : _jobDetail.job_id});
           if( waiterDetail.type === "SEND_ORDER" && waiterDetail.order_id !== null ){
             Orders.update( { _id: waiterDetail.order_id }, 
               { $set: { status: 'ORDER_STATUS.DELIVERED',
@@ -186,11 +200,13 @@ if (Meteor.isServer) {
             { $set : { "status" : "cancel", modification_user : _userId, modification_date : new Date() }
           });
 
-          let waiterDetail = WaiterCallDetails.collection.findOne({job_id : _jobDetail.job_id});
-          if( waiterDetail.type === "CALL_OF_CUSTOMER" && waiterDetail.waiter_id !== '' ){
-            let usr_detail = UserDetails.collection.findOne({ user_id : waiterDetail.waiter_id });
-            let jobs = usr_detail.jobs - 1;
-            UserDetails.update({user_id : waiterDetail.waiter_id} , { $set : { "enabled" : true, "jobs" : jobs } });
+          let waiterDetail = WaiterCallDetails.findOne({job_id : _jobDetail.job_id});
+          if( waiterDetail.type === "CALL_OF_CUSTOMER" && waiterDetail.waiter_id){
+            let usr_detail = UserDetails.findOne({ user_id : waiterDetail.waiter_id });
+            if(usr_detail){
+              let jobs = usr_detail.jobs - 1;
+              UserDetails.update({user_id : waiterDetail.waiter_id} , { $set : { "enabled" : true, "jobs" : jobs } });
+            }
           }
         });
       });
@@ -217,7 +233,7 @@ if (Meteor.isServer) {
       var count =  waiterEnableds.count();
 
       if( count > 0 ) {
-        let restaurantTurn = RestaurantTurns.collection.findOne({ "restaurant_id" : _restaurant },
+        let restaurantTurn = RestaurantTurns.findOne({ "restaurant_id" : _restaurant },
           {
             sort : {"creation_date":-1}
           }
