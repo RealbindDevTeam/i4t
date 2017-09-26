750541b5fd8ab81b86f609b624a0bb48c91306b9
Leonardo González <clgrhc@gmail.com>
2017-09-25 19:15:26 -0500

    finished order item button functionality, checking for addtion button...

--- i4t_mobile/src/pages/customer/orders/orders.ts
+++ i4t_mobile/src/pages/customer/orders/orders.ts
@@ -18,6 +18,7 @@ import { SectionsPage } from '../sections/sections';
 import { ItemEditPage } from '../item-edit/item-edit';
 import { AdditionEditPage } from '../addition-edit/addition-edit';
 import { Currencies } from 'qmo_web/both/collections/general/currency.collection';
+import { ItemImagesThumbs } from 'qmo_web/both/collections/administration/item.collection';
 
 @Component({
     selector: 'page-orders',
@@ -25,41 +26,44 @@ import { Currencies } from 'qmo_web/both/collections/general/currency.collection
 })
 export class OrdersPage implements OnInit, OnDestroy {
 
-    private _userDetailSub      : Subscription;
-    private _restaurantSub      : Subscription;
-    private _tablesSub          : Subscription;
-    private _ordersSub          : Subscription;
-    private _additionsSub       : Subscription;
-    private _itemsSub           : Subscription;
-    private _restaurantThumbSub : Subscription;
-    private _currencySub        : Subscription;
+    private _userDetailSub: Subscription;
+    private _restaurantSub: Subscription;
+    private _tablesSub: Subscription;
+    private _ordersSub: Subscription;
+    private _additionsSub: Subscription;
+    private _itemsSub: Subscription;
+    private _restaurantThumbSub: Subscription;
+    private _currencySub: Subscription;
+    private _imageThumbSub: Subscription;
 
-    private _userLang      : string;
-    private _table_code    : string = "";
-    private _res_code      : string = "";
-    private _statusArray   : string[];
-    private _currentUserId : string;
-    private _orderIndex    : number = -1;
-    private selected       : string = "";
-    private _currencyCode  : string = "";
+    private _userLang: string;
+    private _table_code: string = "";
+    private _res_code: string = "";
+    private _statusArray: string[];
+    private _currentUserId: string;
+    private _orderIndex: number = -1;
+    private selected: string = "";
+    private _currencyCode: string = "";
 
-    private _restaurants : any;
-    private _table       : any;
-    private _additions   : any;
+    private _restaurants: any;
+    private _table: any;
+    private _additions: any;
     private _userDetail;
     private _orders;
     private _allOrders;
     private _items;
 
+    private _currentOrderUserId: string;
+    private _btnOrderItem: boolean = true;
 
-    constructor( public _navCtrl: NavController, 
-                 public _navParams: NavParams, 
-                 public _app: App, 
-                 public _translate: TranslateService,
-                 public alertCtrl: AlertController, 
-                 public _loadingCtrl: LoadingController ,
-                 private _userLanguageService: UserLanguageService,
-                 private _ngZone: NgZone ) {
+    constructor(public _navCtrl: NavController,
+        public _navParams: NavParams,
+        public _app: App,
+        public _translate: TranslateService,
+        public alertCtrl: AlertController,
+        public _loadingCtrl: LoadingController,
+        private _userLanguageService: UserLanguageService,
+        private _ngZone: NgZone) {
         _translate.setDefaultLang('en');
         this._currentUserId = Meteor.userId();
         this._statusArray = ['ORDER_STATUS.REGISTERED', 'ORDER_STATUS.IN_PROCESS', 'ORDER_STATUS.PREPARED'];
@@ -75,18 +79,18 @@ export class OrdersPage implements OnInit, OnDestroy {
         this.init();
     }
 
-    init(){
-        this._translate.use( this._userLanguageService.getLanguage( Meteor.user() ) );        
+    init() {
+        this._translate.use(this._userLanguageService.getLanguage(Meteor.user()));
         this._userDetailSub = MeteorObservable.subscribe('getUserDetailsByUser', Meteor.userId()).subscribe(() => {
             this._ngZone.run(() => {
                 this._userDetail = UserDetails.findOne({ user_id: Meteor.userId() });
                 this._res_code = this._userDetail.current_restaurant;
-                if(this._userDetail){
+                if (this._userDetail) {
                     this._restaurantSub = MeteorObservable.subscribe('getRestaurantByCurrentUser', Meteor.userId()).subscribe(() => {
                         this._ngZone.run(() => {
                             this._tablesSub = MeteorObservable.subscribe('getTableById', this._userDetail.current_table).subscribe();
-                            this._restaurants = Restaurants.findOne({ _id : this._userDetail.current_restaurant });
-                            this._table = Tables.findOne({ _id : this._userDetail.current_table });
+                            this._restaurants = Restaurants.findOne({ _id: this._userDetail.current_restaurant });
+                            this._table = Tables.findOne({ _id: this._userDetail.current_table });
                         });
                     });
                 }
@@ -97,18 +101,15 @@ export class OrdersPage implements OnInit, OnDestroy {
             this._orders = Orders.find({ status: { $in: this._statusArray } });
             this._allOrders = Orders.find({ status: { $in: this._statusArray } });
         });
-        this._itemsSub = MeteorObservable.subscribe('itemsByUser', Meteor.userId()).subscribe(() => {
-            this._items = Items.find({});
-        });
-        this._additionsSub = MeteorObservable.subscribe('additionsByCurrentRestaurant', Meteor.userId()).subscribe(() => {
-            this._additions = Additions.find({}).zone();
-        });
+        this._itemsSub = MeteorObservable.subscribe('itemsByUser', Meteor.userId()).subscribe();
+        this._additionsSub = MeteorObservable.subscribe('additionsByCurrentRestaurant', Meteor.userId()).subscribe();
 
         this._restaurantThumbSub = MeteorObservable.subscribe('restaurantImageThumbsByUserId', Meteor.userId()).subscribe();
+        this._imageThumbSub = MeteorObservable.subscribe('itemImageThumbsByUserId', Meteor.userId()).subscribe();
 
         this._currencySub = MeteorObservable.subscribe('getCurrenciesByCurrentUser', Meteor.userId()).subscribe(() => {
             this._ngZone.run(() => {
-                if(Currencies.find({}).fetch().length > 0) {
+                if (Currencies.find({}).fetch().length > 0) {
                     this._currencyCode = Currencies.find({}).fetch()[0].code;
                 }
             });
@@ -265,13 +266,13 @@ export class OrdersPage implements OnInit, OnDestroy {
         alertConfirm.present();
     }
 
-    goToItemEdit(event, _order: any) {
+    goToItemEdit(_itemId: string, _orderItemIndex: number, _order: Order) {
         let loader = this._loadingCtrl.create({
             duration: 300
         });
         loader.present();
-        let objaux: any[] = [];
-        objaux = event;
+        //let objaux: any[] = [];
+        //objaux = event;
         let _lUserDetail = UserDetails.findOne({ user_id: Meteor.userId() });
 
         if (_lUserDetail.current_restaurant && _lUserDetail.current_table) {
@@ -281,8 +282,8 @@ export class OrdersPage implements OnInit, OnDestroy {
 
         this._navCtrl.push(ItemEditPage, {
             order_id: _order._id,
-            item_ord_ind: objaux[1],
-            item_code: objaux[0],
+            item_ord_ind: _orderItemIndex,
+            item_code: _itemId,
             creation_user: _order.creation_user,
             res_code: this._res_code,
             table_code: this._table_code
@@ -300,6 +301,41 @@ export class OrdersPage implements OnInit, OnDestroy {
         }
         this._navCtrl.push(AdditionEditPage, { order_addition: _pAdition, order: _pOrder, restaurant: this._res_code });
     }
+
+
+    getItemThumb(_itemId: string): string {
+        let _imageThumb = ItemImagesThumbs.findOne({ itemId: _itemId }).url;
+        if (_imageThumb) {
+            return _imageThumb;
+        } else {
+            return 'assets/img/default-plate.png';
+        }
+    }
+
+    getItemName(_itemId: string): string {
+        let _itemName = Items.findOne({ _id: _itemId }).name;
+        if (_itemName) {
+            return _itemName;
+        }
+    }
+
+    /**
+    * Function to get item avalaibility 
+    */
+    getItemAvailability(itemId: string): boolean {
+        let _item = Items.find().fetch().filter((i) => i._id === itemId)[0];
+        if (_item) {
+            return (_item.restaurants.filter(r => r.restaurantId === this._res_code)[0]).isAvailable;
+        }
+    }
+
+    getAdditionName(_additionId: string): string {
+        let _additionName = Additions.findOne({ _id: _additionId }).name;
+        if (_additionName) {
+            return _additionName;
+        }
+    }
+
 
     ionViewDidEnter() {
     }
@@ -331,13 +367,15 @@ export class OrdersPage implements OnInit, OnDestroy {
     /**
      * Remove all subscriptions
      */
-    removeSubscriptions():void{
-        if(this._restaurantSub) {this._restaurantSub.unsubscribe();}
-        if(this._ordersSub) {this._ordersSub.unsubscribe();}
-        if(this._tablesSub) {this._tablesSub.unsubscribe();}
-        if(this._itemsSub) {this._itemsSub.unsubscribe();}
-        if(this._restaurantThumbSub) {this._restaurantThumbSub.unsubscribe();}
-        if(this._userDetailSub) {this._userDetailSub.unsubscribe();}
-        if(this._currencySub) {this._currencySub.unsubscribe();}
+    removeSubscriptions(): void {
+        if (this._restaurantSub) { this._restaurantSub.unsubscribe(); }
+        if (this._ordersSub) { this._ordersSub.unsubscribe(); }
+        if (this._tablesSub) { this._tablesSub.unsubscribe(); }
+        if (this._itemsSub) { this._itemsSub.unsubscribe(); }
+        if (this._restaurantThumbSub) { this._restaurantThumbSub.unsubscribe(); }
+        if (this._userDetailSub) { this._userDetailSub.unsubscribe(); }
+        if (this._currencySub) { this._currencySub.unsubscribe(); }
+        if (this._imageThumbSub) { this._imageThumbSub.unsubscribe(); }
+        if (this._additionsSub) { this._additionsSub.unsubscribe(); }
     }
 }
