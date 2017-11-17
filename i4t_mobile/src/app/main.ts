a9a1407b463f3630fae96c765c10cdbb102bfdc4
leonardoespinosa <leonardoespinosa@MacBook-Pro-de-Leonardo.local>
2017-11-17 17:55:08 -0500

    Remove old mobile project

--- i4t_mobile/src/app/main.ts
+++
@@ -1,8 +0,0 @@
-import '../../meteor-client';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app.module';
-import { enableProdMode } from '@angular/core';
-
-// this is the magic wand
-enableProdMode();
-platformBrowserDynamic().bootstrapModule(AppModule);
