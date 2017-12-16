57680c013484423a2e770f2b671d56a98a056074
Leonardo González <clgrhc@gmail.com>
2017-12-16 14:56:06 -0500

    finished changes on web and mobile projects

--- i4t_web/server/imports/publications/restaurant/invoice.ts
+++ i4t_web/server/imports/publications/restaurant/invoice.ts
@@ -6,15 +6,15 @@ import { Invoices } from '../../../../both/collections/restaurant/invoice.collec
  * This method return Invoices by UserId
  * @param {string} _pUserId
  */
-Meteor.publish('getInvoicesByUserId', function( _pUserId : string ){
+Meteor.publish('getInvoicesByUserId', function (_pUserId: string) {
     check(_pUserId, String);
-    return Invoices.find({ customer_id : _pUserId });
+    return Invoices.find({ customer_id: _pUserId }, { sort: { creation_date: -1 } });
 });
 
 /**
  * Meteor publication invoices with restaurant Ids
  * @param {string[]} _pRestaurantIds
  */
-Meteor.publish( 'getInvoicesByRestaurantIds', function( _pRestaurantIds: string[] ) {
-    return Invoices.find( { restaurant_id: { $in: _pRestaurantIds } } );
+Meteor.publish('getInvoicesByRestaurantIds', function (_pRestaurantIds: string[]) {
+    return Invoices.find({ restaurant_id: { $in: _pRestaurantIds } });
 });
